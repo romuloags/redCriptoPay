dab9c941f8db057369ead44ec2b8b939e90cd854
romulo <romulo2305@gmail.com>
2021-12-28 09:52:01 -0400

    deleted space between div and img

--- frontend/src/components/tokensTransactions.js
+++ frontend/src/components/tokensTransactions.js
@@ -11,24 +11,24 @@ const tokensTransactions =[
     labelSingle: <span><img className="me-2 mb-1" src={usdticon} 
     width="20" height="20" alt="usdticon"/>USDT</span>},
     { address: "0x3103D92d2888B7Aeee60C59a72e4Ba6A3146C9d0", 
-    tag: 'BUSD', label: <div> <img className="me-2 mb-1" src={busdicon} 
+    tag: 'BUSD', label: <div><img className="me-2 mb-1" src={busdicon} 
     width="20" height="20" alt="busdicon"/>BUSD</div>,
-    labelSingle: <span> <img className="me-2 mb-1" src={busdicon} 
+    labelSingle: <span><img className="me-2 mb-1" src={busdicon} 
     width="20" height="20" alt="busdicon"/>BUSD</span> },
     { address: "0xbFe4f8A71C8C3E1b748D42ACfB2F1140931f50bA",
-     tag: 'USDC', label: <div> <img className="me-2 mb-1" src={usdcicon} 
+     tag: 'USDC', label: <div><img className="me-2 mb-1" src={usdcicon} 
     width="20" height="20" alt="usdcicon"/>USDC</div>,
-    labelSingle: <span> <img className="me-2 mb-1" src={usdcicon} 
+    labelSingle: <span><img className="me-2 mb-1" src={usdcicon} 
     width="20" height="20" alt="usdcicon"/>USDC</span> },
     { address: "0x3aFf2aB4cd7cF0f08B54E06D943dcDe8719cfd82", 
-    tag: 'BTCB', label:<div> <img className="me-2 mb-1" src={btcbicon} 
+    tag: 'BTCB', label:<div><img className="me-2 mb-1" src={btcbicon} 
     width="20" height="20" alt="btcbicon"/>BTCB</div>,
-    labelSingle:<span> <img className="me-2 mb-1" src={btcbicon} 
+    labelSingle:<span><img className="me-2 mb-1" src={btcbicon} 
     width="20" height="20" alt="btcbicon"/>BTCB</span> },
     { address: "0xdFabe06028A1008354ae8Ed8F8E63202A0A11853", 
-    tag: 'ETH', label: <div> <img className="me-2 mb-1" src={ethicon} 
+    tag: 'ETH', label: <div><img className="me-2 mb-1" src={ethicon} 
     width="20" height="20" alt="usdticon"/>ETH</div>,
-    labelSingle: <span> <img className="me-2 mb-1" src={ethicon} 
+    labelSingle: <span><img className="me-2 mb-1" src={ethicon} 
     width="20" height="20" alt="usdticon"/>ETH</span> }
     ];
 
