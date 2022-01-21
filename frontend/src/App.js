0881a8963332fcd4c2e7e2ae58b4234bd5a5095c
romulo <romulo2305@gmail.com>
2022-01-21 16:29:37 -0400

    added whitepaper info & terms & _redirect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,10 +21,11 @@ import SingleBNBreceiver from "./components/singleBNBreceiver";
 import SingleTOKENSsender from "./components/singleTOKENSsender";
 import SingleTOKENSreceiver from "./components/singleTOKENSreceiver";
 import Error404 from "./components/404";
-import WhitePaper from "./components/whitepaper"
-import Support from "./components/support"
+import WhitePaper from "./components/whitepaper";
+import Support from "./components/support";
+import Terms from "./components/terms";
 
-import "./custom.css"
+import "./custom.css";
 
 function App() {
 
@@ -183,6 +184,9 @@ setIsLoading(false);
           </Route>
           <Route exact path="/whitepaper" >
            <WhitePaper/>
+          </Route>
+          <Route exact path="/terminos" >
+           <Terms/>
           </Route>
           <Route exact path="/soporte" >
            <Support/>
