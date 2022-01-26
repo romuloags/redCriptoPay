7703c311d0964ca37e18f0026059afa582d7c053
romulo <romulo2305@gmail.com>
2022-01-26 19:30:20 -0400

    error 404 modified & footer now not rounded at top

--- frontend/src/components/404.js
+++ frontend/src/components/404.js
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
 
 const Error404 = () => {
-    return (   
-    <div className="position-absolute top-50 start-50 translate-middle justify-content-center">
-       <h1>Error 404</h1>
-       <div>
-       <Link to="/">
-       <button className="btn btn-dark  m-3 m-lg-5 justify-content-center" type="submit">    
-       <span className="text-danger"><i className="bi bi-house pe-2"></i>Home</span>
-       </button>
-       </Link>
-       </div>
-
+    return (
+    <div className="min-vh-100"> 
+      <section className="py-0 my-0">
+        <div className="container-md">       
+         <div className=" position-absolute top-50 start-50 translate-middle justify-content-center">
+         <h1 className="text-muted">Error 404</h1>
+         <div>
+         <Link to="/">
+         <button className="btn btn-primary  m-3 m-lg-5 justify-content-center" type="submit">    
+         <span><i className="bi bi-house pe-2"></i>Home</span>
+         </button>
+         </Link>
+         </div>
+         </div>
+        </div>
+      </section>
     </div>
     );
 }
