a719d5e3fdc87e483b8e247160d6f7572ff39cd7
romulo <romulo2305@gmail.com>
2022-03-02 20:30:06 -0400

    possibility to deploy contracts in remix

--- contracts/BNBescrow.sol
+++ contracts/BNBescrow.sol
@@ -4,7 +4,44 @@ pragma solidity ^0.8.4;
 
 //redcriptopay
 
-import "./Ownable.sol";
+pragma solidity ^0.8.0;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+contract Ownable is Context {
+    
+    address public owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor() {
+        _setOwner(_msgSender());
+    }
+
+    modifier onlyOwner() {
+        require(owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _setOwner(newOwner);
+    }
+
+    function _setOwner(address newOwner) private {
+        address oldOwner = owner;
+        owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
 
 /**
  * @dev Contract module that helps prevent reentrant calls to a function.
